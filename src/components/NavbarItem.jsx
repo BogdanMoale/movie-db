a69da84e5c1340725acdb6c3c32b6c48a1767adb
BogdanMoale <moale.bogdan.ionica@gmail.com>
2024-08-18 12:30:34 +0300

    add suspense boundary

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -2,22 +2,30 @@
 
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-export default function NavbarItem({ title, param }) {
+function NavbarItemContent({ title, param }) {
   const searchParams = useSearchParams();
   const genre = searchParams.get("genre");
+
   return (
-    <div>
-      <Link
-        className={`hover:text-amber-600 font-semibold ${
-          genre === param
-            ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
-            : ""
-        }`}
-        href={`/?genre=${param}`}
-      >
-        {title}
-      </Link>
-    </div>
+    <Link
+      className={`hover:text-amber-600 font-semibold ${
+        genre === param
+          ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+          : ""
+      }`}
+      href={`/?genre=${param}`}
+    >
+      {title}
+    </Link>
+  );
+}
+
+export default function NavbarItem({ title, param }) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <NavbarItemContent title={title} param={param} />
+    </Suspense>
   );
 }
